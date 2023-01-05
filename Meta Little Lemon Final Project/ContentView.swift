96f52074d73a19c36713ab3ad56f2aad4e04ddcb
onurfeyyaz <feyyazonur0@gmail.com>
2023-01-05 23:14:51 +0300

    Initial Commit

---
+++ Meta Little Lemon Final Project/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Meta Little Lemon Final Project
+//
+//  Created by Feyyaz ONUR on 16.12.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("initial commit")
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Meta Little Lemon Final Project/Meta_Little_Lemon_Final_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Meta_Little_Lemon_Final_ProjectApp.swift
+//  Meta Little Lemon Final Project
+//
+//  Created by Feyyaz ONUR on 16.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct Meta_Little_Lemon_Final_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
